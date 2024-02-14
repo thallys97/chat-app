647e44f6f4d49c4d49012871885996b65652346a
thallys97 <thallysbandeira97@gmail.com>
2024-02-14 08:29:14 -0500

    atualizei o server.js para integrar o Socket.io

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server); // Inicia o Socket.io com o servidor HTTP
+
 const PORT = 3000;
 
-// Middleware para servir arquivos estáticos
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Envia a mensagem para todos os usuários conectados
+    });
 });
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+

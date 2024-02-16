8a535cf8cb6db67c09c0575a8e9b616b99d396ff
thallys97 <thallysbandeira97@gmail.com>
2024-02-16 12:52:52 -0500

    inclui a entrada de tipo no schema de mensagem

--- messageModel.js
+++ messageModel.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
     username: String,
     message: String,
+    type: { type: String, default: 'regular' }, // 'info' para mensagens do sistema
     timestamp: { type: Date, default: Date.now }
 });
+
 
 module.exports = mongoose.model('Message', messageSchema);

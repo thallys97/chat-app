8b63f063e3cfffc14c2788326cd44eb1e00c492d
thallys97 <thallysbandeira97@gmail.com>
2024-02-18 09:54:13 -0500

    adicionei novos campos ao schema da mensagem

--- messageModel.js
+++ messageModel.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
     username: String,
-    message: String,
-    type: { type: String, default: 'regular' }, // 'info' para mensagens do sistema
+    text: String, // Mensagem de texto
+    image: String, // URL da imagem
+    video: String, // URL do vídeo
+    link: String, // URL do link
+    type: { type: String, default: 'regular' }, // Tipo de mensagem: 'regular', 'info', 'image', 'video', 'link'
     timestamp: { type: Date, default: Date.now }
 });
 

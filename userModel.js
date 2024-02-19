fa94e8f4f79e52977975886b231a561771b01f2e
thallys97 <thallysbandeira97@gmail.com>
2024-02-19 12:18:11 -0500

    criando modelo de usuário com username e um ID único

---
+++ userModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    userID: { type: String, unique: true }
+});
+
+module.exports = mongoose.model('User', userSchema);

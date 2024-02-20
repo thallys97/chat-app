aa7687dc62b95e54e3bdcebacb026ff4c8e97b24
thallys97 <thallysbandeira97@gmail.com>
2024-02-20 10:50:17 -0500

    atualizei userModel.js para criar um hash de senha

--- userModel.js
+++ userModel.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    userID: { type: String, unique: true }
+    username: { type: String, unique: true },
+    password: String,
+    // userID não é mais necessário, pois o mongoose cria um _id por padrão
+});
+
+// Hashing a password before saving it to the database
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);

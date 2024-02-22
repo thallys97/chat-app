921c0a1f0daf2252f739dd9430d077c3f4c129e4
thallys97 <thallysbandeira97@gmail.com>
2024-02-22 14:31:26 -0500

    desenvolvi a lógica para checar se meu usuário está autenticado e também para realizar o logout do chat

--- public/script.js
+++ public/script.js
@@ -3,10 +3,122 @@ import { EmojiButton } from 'https://unpkg.com/@joeattardi/emoji-button@latest';
 
 const socket = io(); // Inicia a conexão WebSocket com o servidor
 
-document.addEventListener('DOMContentLoaded', function() {
-    const usernameInput = document.getElementById('username');
-    const setUsernameBtn = document.getElementById('set-username-btn');
+// Função para verificar se o usuário está logado
+function checkAuth() {
+    const token = localStorage.getItem('token');
+    const authContainer = document.getElementById('auth-container');
     const logoutBtn = document.getElementById('logout-btn');
+    const messageForm = document.getElementById('message-form');
+    const messageInput = document.getElementById('message-input');
+    const messagesContainer = document.getElementById('messages');
+    const emojiButton = document.querySelector('#emoji-button');
+    
+    if (token) {
+        // Se o token existir, assumimos que o usuário está logado
+        authContainer.style.display = 'none';
+        messageForm.style.display = 'flex';
+        emojiButton.style.display = 'inline-block'; // ou 'block', dependendo do seu layout
+        logoutBtn.style.display = 'block';
+        messageInput.focus();
+    } else {
+        // Se não houver token, o usuário não está logado
+        logoutBtn.style.display = 'none';
+        authContainer.style.display = 'block';
+        messageForm.style.display = 'none';
+        emojiButton.style.display = 'none';
+    }
+}
+
+// // Adicione funções para lidar com login e registro
+// async function login(username, password) {
+//     // Implemente a lógica de envio de dados de login para o servidor e armazenamento do token
+// }
+
+// async function register(username, password) {
+//     // Implemente a lógica de envio de dados de registro para o servidor
+// }
+
+async function logout() {
+    try {
+        const token = localStorage.getItem('token');
+        // Envia uma solicitação de logout ao servidor
+        const response = await fetch('/logout', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}` // Envia o token no cabeçalho da requisição
+            }
+        });
+
+        // Verifica se o logout foi bem-sucedido
+        if (response.ok) {
+            // Remove o token e o username do armazenamento local
+            localStorage.removeItem('token');
+            localStorage.removeItem('username');
+
+            // Desconecta o socket ao fazer logout
+            socket.disconnect();
+
+            // Recarrega a página
+            window.location.reload();
+        } else {
+            // Lida com qualquer erro que possa ter ocorrido durante o logout
+            console.error('Falha ao realizar logout');
+        }
+    } catch (error) {
+        console.error('Erro ao fazer logout:', error);
+    }
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Verifica se o usuário está logado
+
+    checkAuth();
+
+      
+    const token = localStorage.getItem('token');
+    if (token) {
+        const socket = io();
+        
+        socket.on('connect', () => {
+        // Autentica o usuário assim que a conexão WebSocket é estabelecida
+        socket.emit('authenticate', { token: token });
+        });
+        
+        socket.on('authenticated', (data) => {
+        // Atualiza a UI com o nome de usuário ou realiza outras ações necessárias
+        console.log('Usuário autenticado:', data.username);
+        });
+    }
+
+    const loginBtn = document.getElementById('login-btn');
+    const registerBtn = document.getElementById('register-btn');
+    const logoutBtn = document.getElementById('logout-btn');
+    
+    loginBtn.addEventListener('click', function() {
+        window.location.href = 'login.html'; // Redireciona para a página de login
+    });
+
+    registerBtn.addEventListener('click', function() {
+        window.location.href = 'register.html'; // Redireciona para a página de registro
+    });
+
+    // logoutBtn.addEventListener('click', function() {
+    //     // Limpar o token do localStorage e atualizar a interface
+    //     localStorage.removeItem('token');
+    //     localStorage.removeItem('username');
+    //     checkAuth();
+    // });
+
+    logoutBtn.addEventListener('click', logout );
+
+
+    //const usernameInput = document.getElementById('username');
+    //const setUsernameBtn = document.getElementById('set-username-btn');
+    //const logoutBtn = document.getElementById('logout-btn');
     const messageForm = document.getElementById('message-form');
     const messageInput = document.getElementById('message-input');
     const messagesContainer = document.getElementById('messages');
@@ -24,53 +136,61 @@ document.addEventListener('DOMContentLoaded', function() {
     emojiButton.addEventListener('click', () => picker.togglePicker(emojiButton));
 
 
-    let username = localStorage.getItem('username') || '';
+    //let username = localStorage.getItem('username') || '';
 
 
-    if (username) {
-        // Se o nome de usuário estiver definido, mostra o form e o botão de emoji
-        messageForm.style.display = 'flex';
-        emojiButton.style.display = 'inline-block'; // ou 'block', dependendo do seu layout
-        usernameInput.style.display = 'none';
-        setUsernameBtn.style.display = 'none';
-        logoutBtn.style.display = 'block';
-        messageInput.focus();
-    } else {
-        // Se o nome de usuário não estiver definido, esconde o form e o botão de emoji
-        messageForm.style.display = 'none';
-        emojiButton.style.display = 'none';
-        logoutBtn.style.display = 'none';
-    }
+    // if (username) {
+    //     // Se o nome de usuário estiver definido, mostra o form e o botão de emoji
+    //     messageForm.style.display = 'flex';
+    //     emojiButton.style.display = 'inline-block'; // ou 'block', dependendo do seu layout
+    //     usernameInput.style.display = 'none';
+    //     setUsernameBtn.style.display = 'none';
+    //     logoutBtn.style.display = 'block';
+    //     messageInput.focus();
+    // } else {
+    //     // Se o nome de usuário não estiver definido, esconde o form e o botão de emoji
+    //     messageForm.style.display = 'none';
+    //     emojiButton.style.display = 'none';
+    //     logoutBtn.style.display = 'none';
+    // }
+
+
+
+
+
 
     // Função para definir o nome de usuário
-    function setUsername() {
-        username = usernameInput.value.trim();
-        if (username) {
-            localStorage.setItem('username', username); // Armazena o nome de usuário no localStorage
-            socket.emit('set username', username); // Notify the server of the new username
-            usernameInput.style.display = 'none';
-            setUsernameBtn.style.display = 'none';
-            logoutBtn.style.display = 'block';
-            messageInput.focus();
-        } else {
-            alert('Por favor, insira um nome de usuário válido.');
-        }
-    }
+    // function setUsername() {
+    //     username = usernameInput.value.trim();
+    //     if (username) {
+    //         localStorage.setItem('username', username); // Armazena o nome de usuário no localStorage
+    //         socket.emit('set username', username); // Notify the server of the new username
+    //         usernameInput.style.display = 'none';
+    //         setUsernameBtn.style.display = 'none';
+    //         logoutBtn.style.display = 'block';
+    //         messageInput.focus();
+    //     } else {
+    //         alert('Por favor, insira um nome de usuário válido.');
+    //     }
+    // }
 
-    // Adiciona um ouvinte de evento ao botão de definir nome de usuário
-    setUsernameBtn.addEventListener('click', function () {
-        setUsername();
-        // socket.emit('set username', username);
-        window.location.reload();
-    });
+    // // Adiciona um ouvinte de evento ao botão de definir nome de usuário
+    // setUsernameBtn.addEventListener('click', function () {
+    //     setUsername();
+    //     // socket.emit('set username', username);
+    //     window.location.reload();
+    // });
 
 
-    // Adiciona um ouvinte de evento ao botão de sair
-    logoutBtn.addEventListener('click', function() {
-        socket.emit('logout', username); // Optionally notify the server that the user is logging ou
-        localStorage.removeItem('username'); // Remove o nome de usuário do localStorage
-        window.location.reload(); // Recarrega a página para redefinir o estado
-    });
+    // // Adiciona um ouvinte de evento ao botão de sair
+    // logoutBtn.addEventListener('click', function() {
+    //     socket.emit('logout', username); // Optionally notify the server that the user is logging ou
+    //     localStorage.removeItem('username'); // Remove o nome de usuário do localStorage
+    //     window.location.reload(); // Recarrega a página para redefinir o estado
+    // });
+
+    
+
 
 
     // Função para fazer upload de arquivos
@@ -104,17 +224,20 @@ document.addEventListener('DOMContentLoaded', function() {
     
         const messageText = messageInput.value.trim();
         const file = document.getElementById('file-input').files[0];
+        const username = localStorage.getItem('username');
     
-        if (messageText && username) {
+        if (messageText) {
+
+            
             // Se houver texto, envia uma mensagem de texto
-            socket.emit('chat message', { username: username, text: messageText });
+            socket.emit('chat message', {username: username, text: messageText });
             messageInput.value = '';
         } else if (file) {
             // Se houver um arquivo, você precisa fazer upload para um servidor ou serviço de armazenamento de arquivos
             // Depois de fazer upload do arquivo e obter a URL, você pode enviar essa URL como uma mensagem
             uploadFile(file).then(response => {
                 const url = response.url;
-                socket.emit('chat message', { username: username, file: url, type: file.type.startsWith('image/') ? 'image' : 'video' });
+                socket.emit('chat message', {username: username, file: url, type: file.type.startsWith('image/') ? 'image' : 'video' });
                 messageInput.value = '';
                 document.getElementById('file-input').value = ''; // Limpar o campo de arquivo
             }).catch(error => {
@@ -136,7 +259,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Função para exibir mensagens
     function displayMessage(data) {  
 
-        console.log(data);
+        //console.log(data);
 
         const messageElement = document.createElement('div');
     

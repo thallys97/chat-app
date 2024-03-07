891e40115aae500e1050a32b557b40d14a6c1a41
thallys97 <thallysbandeira97@gmail.com>
2024-03-07 09:40:09 -0500

    adicionei a entrada de createdBy no model de canal

--- channelModel.js
+++ channelModel.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const channelSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     topic: { type: String, required: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }] // Adicionando campo de mensagens
 });
